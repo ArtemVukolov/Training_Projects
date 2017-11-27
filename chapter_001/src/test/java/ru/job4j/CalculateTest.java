6d1d2aaa856b5ee350578e49cd076574e46ea10c
Artem Vukolov <avukolov@me.com>
2017-11-27 15:17:59 +0300

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Artem Vukolov (avukolov@me.com)
+* @version 1.0
+* @since 27.11.2017
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/ @Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Petr Arsentev";
+    String expect = "Echo, echo, echo : Petr Arsentev"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate test.
+*
+* @author Artem Vukolov (avukolov@me.com)
+* @version 1.0
+* @since 27.11.2017
+*/
+package ru.job4j;
