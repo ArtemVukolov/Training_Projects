619632fde8eb004dcb40a6d1320368520ae21306
Artem Vukolov <avukolov@me.com>
2017-12-01 01:40:50 +0300

    3.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,15 @@
+package ru.job4j.max;
+
+/**
+ * Максимум из двух чисел.
+ *
+ * @author Artem Vukolov (avukolov@me.com)
+ * @version $Id
+ * @since 01.12.2017
+ */
+
+public class Max {
+    public int max(int first, int second) {
+    return false ? first : second;
+    }
+}
