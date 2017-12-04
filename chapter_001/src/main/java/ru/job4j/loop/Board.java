c73be256ab0335aea43008c5ddf24a99b497fb98
Artem Vukolov <avukolov@me.com>
2017-12-04 19:33:58 +0300

    Check validate.

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -11,9 +11,9 @@ package ru.job4j.loop;
 public class Board {
     public String paint(int width, int height) {
         StringBuilder builder = new StringBuilder();
-        for (int i = 1; i <= height; i++){
-            for (int j = 1; j <= width; j++){
-                if ((i + j) % 2 == 0){
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
+                if ((i + j) % 2 == 0) {
                     builder.append("X");
                 } else {
                     builder.append(" ");
